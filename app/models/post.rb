9e15462d9d97121ac7494f2d7b52af404b7ab033
Bettenoch <bettenoch254@gmail.com>
2023-11-02 09:24:52 -0700

    Remove linters errors.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,30 +1,28 @@
 class Post < ApplicationRecord
-    # belongs_to :author, class_name: 'User'
-    has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :post_counter
-  
-    # attribute :title, :string
-    # attribute :text, :text
-    # attribute :comments_counter, :integer, default: 0
-    # attribute :likes_counter, :integer, default: 0
-    # attribute :posts_counter, :integer, default: 0
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :posts_counter
 
-    after_save :increment_post_counter
-  
-    def top_five__comments
-      comments.order(created_at: :desc).limit(5)
-    end
-  
-    # A method to increment the likes_counter attribute.
-    # def increment_likes_counter
-    #   update(likes_counter: likes_counter + 1)
-    # end
-   
-    private
-  
-    def increment_post_counter
-      author.increment!(:posts_counter)
-    end
+  # attribute :title, :string
+  # attribute :text, :text
+  # attribute :comments_counter, :integer, default: 0
+  # attribute :likes_counter, :integer, default: 0
+  # attribute :posts_counter, :integer, default: 0
+
+  after_save :increment_post_counter
+
+  def top_five__comments
+    comments.order(created_at: :desc).limit(5)
   end
-  
+
+  # A method to increment the likes_counter attribute.
+  # def increment_likes_counter
+  #   update(likes_counter: likes_counter + 1)
+  # end
+
+  private
+
+  def increment_post_counter
+    author.increment!(:posts_counter)
+  end
+end
